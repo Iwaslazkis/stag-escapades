0815272428d692f31c4e5b98508a298d082c6b51
Diogo Iwasaki <67493856+Iwaslazkis@users.noreply.github.com>
2021-04-23 08:25:05 -0500

    Trying to fix parser.js

--- src/svelte/parser.js
+++ src/svelte/parser.js
@@ -1,12 +1,19 @@
 import fs from 'fs';
+import config from '/home/student/projects/stag-escapades/src/svelte/puzzles.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const md = fs.readFileSync('scenes.md', 'utf-8');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const md = fs.readFileSync(__dirname + '/scenes.md', 'utf-8');
 let parsed = [];
 let id = 0;
 
 md
     .split('\n\n')
     .forEach( (el, i, arr) => {
+        const matched = el.match(/#(.+): (.*)/);
+        console.log(config.activity("hi"));
         if (el.startsWith('#SCENE:')) {
             let scene = {
                 id: id,
@@ -16,7 +23,10 @@ md
 
             parsed.push(scene);
             id++;
-        } else if (!(el.match(/#.+: .*/))) {
+        } else if (el.match(/#.+: .*/)) {
+            //const line = ["", matched[2], config[matched[1]](matched[2])];
+            parsed[parsed.length - 1].lines.push(matched[1]);
+        } else {
             parsed[parsed.length - 1].lines.push(el);
         }
     })
